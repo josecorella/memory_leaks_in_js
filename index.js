39b8c7709a7b7d2f2bde19fe80e1db70cfe5b0a5
Jose Corella <jocorell@amazon.com>
2022-06-13 16:59:26 -0700

    chore: add a little more memory usage

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ function requestListener(req, res) {
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.write(resp)
         res.end()
+    } else if (req.url === '/getSushi') {
+        function veryHardMath() {
+            let endTime = Date.now() + (5 * 1000);
+            while (Date.now() < endTime) {
+                Math.random()
+            }
+        }
+
+        function theSushiTable() {
+            return new Promise(resolve => {
+            resolve('🍣');
+            });
+        }
+
+        async function getSushi() {
+            let sushi = await theSushiTable();
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+            res.write(`Enjoy! ${sushi}`);
+            res.end()
+        }
+        getSushi()
+        importantMath()
+    } else {
+        res.end('Invalid request')
     }
 }
 
