376420e94d02b1c3e90597b10b0b662a59478edb
Jose Corella <jocorell@amazon.com>
2022-06-13 16:53:27 -0700

    chore: add basic websocket layer

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const http = require('http')
+
+const leak = []
+
+function requestListener(req, res) {
+    if (req.url === '/now') {
+        let resp = JSON.stringify({ now: new Date()})
+        leak.push(JSON.parse(resp))
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.write(resp)
+        res.end()
+    }
+}
+
+const server = http.createServer(requestListener)
+server.listen(process.env.PORT || 3000)
