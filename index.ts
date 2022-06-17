35bc4d9e4b33794e607dfae12da41c2444a36102
Jose Corella <jocorell@amazon.com>
2022-06-17 10:25:20 -0700

    chore: make frame size optional for enc stream

--- index.ts
+++ index.ts
@@ -45,11 +45,10 @@ app.get('/readRandom_1gbEnc', (req, res) => {
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
-    
 })
 
-async function readFile(filename:string) {
-    await kmsEncryptStream(filename);
+async function readFile(filename:string, framesize?:number) {
+    await kmsEncryptStream(filename, framesize);
 }
 
 async function readEncryptedFile(filename: string) {
--- memory.ts
+++ memory.ts
@@ -1,8 +1,7 @@
 import {
     KmsKeyringNode,
     buildClient,
-    CommitmentPolicy,
-    MessageHeader,
+    CommitmentPolicy
 } from '@aws-crypto/client-node'
 import {AlgorithmSuiteIdentifier} from '@aws-crypto/material-management'
 
@@ -11,7 +10,7 @@ const { encryptStream, decryptUnsignedMessageStream } = buildClient(
 )
 
 import { finished } from 'stream'
-import { createReadStream, createWriteStream, read } from 'fs'
+import { createReadStream, createWriteStream } from 'fs'
 import { promisify } from 'util'
 const finishedAsync = promisify(finished)
 
@@ -20,11 +19,11 @@ const generatorKeyId =
 const keyring = new KmsKeyringNode({ generatorKeyId })
 const context = {
     stage: 'demo',
-    purpose: 'simple demonstration app',
+    purpose: 'streaming memory stress test',
     origin: 'us-west-2',
 }
 
-export async function kmsEncryptStream(filename:string) {
+export async function kmsEncryptStream(filename:string, framesize?:number) {
     const readable = createReadStream(filename)
     const encFile = filename + '.encrypted'
     const writeable = createWriteStream(encFile)
@@ -33,7 +32,8 @@ export async function kmsEncryptStream(filename:string) {
         encryptStream(keyring, {
             suiteId:
                 AlgorithmSuiteIdentifier.ALG_AES256_GCM_IV12_TAG16_HKDF_SHA512_COMMIT_KEY,
-            encryptionContext: context
+            encryptionContext: context,
+            frameLength: framesize
         })
     ).pipe(writeable.on('finish', () => {
         console.log(`The new file name is ${encFile}.`);
